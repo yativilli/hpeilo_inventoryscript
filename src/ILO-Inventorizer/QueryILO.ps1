0e81c88b3541aed437fb282fcf8f99333ba99b9d
wernle_y <yannick.wernle@git.psi.ch>
2025-03-11 14:54:27 +0100

    Added Memory to Query

--- src/ILO-Inventorizer/QueryILO.ps1
+++ src/ILO-Inventorizer/QueryILO.ps1
@@ -7,62 +7,96 @@ Function Get-DataFromILO {
         [Array]
         $servers
     )
-    Write-Host $servers;
-    Get-Module HPEiLOCmdlets;
+    try {
+        $config = Get-Config;
+        $login = Get-Content -Path $config.loginConfigPath | ConvertFrom-Json -Depth 2;
 
-    $config = Get-Config;
-    $login = Get-Content -Path $config.loginConfigPath | ConvertFrom-Json -Depth 2;
+        Log 0 "Started"
+        $report = @();
+        foreach ($srv in $servers) {
 
-    $report = @();
-    foreach ($srv in $servers) {
-
-        $findILO = Find-HPEiLO $srv;
-        $conn = Connect-HPEiLO -Address $srv -Username $login.Username -Password $login.Password -DisableCertificateAuthentication:($config.deactivateCertificateValidation);
+            $findILO = Find-HPEiLO $srv;
+            $iLOVersion = ([regex]"\d").Match($findILO.PN).Value;
+            $conn = Connect-HPEiLO -Address $srv -Username $login.Username -Password $login.Password -DisableCertificateAuthentication:($config.deactivateCertificateValidation);
        
-        $healthSummary = ($conn | Get-HPEiLOHealthSummary);
-        $powerSupply = ($conn | Get-HPEiLOPowerSupply);
-
-        $srvReport = [ordered]@{
-            Serial         = $findILO.SerialNumber;
-            Part_Type_Name = $conn.TargetInfo.ProductName;
-            Hostname       = ($conn | Get-HPEiLOAccessSetting).ServerName.ToLower();
-            Hostname_Mgnt  = $conn.Hostname.ToLower();
-            MAC_1          = $conn;
-            MAC_2          = $conn;
-            MAC_3          = $conn;
-            MAC_4          = $conn;
-            Mgnt_MAC       = ($conn | Get-HPEiLOIPv4NetworkSetting).PermanentMACAddress.ToLower();
-
-            Health_Summary = @{
-                FanStatus           = $healthSummary.FanStatus;
-                MemoryStatus        = $healthSummary.MemoryStatus;
-                PowerSuppliesStatus = $healthSummary.PowerSuppliesStatus;
-                ProcessorStatus     = $healthSummary.ProcessorStatus;
-                StorageStatus       = $healthSummary.StorageStatus;
-                TemperatureStatus   = $healthSummary.TemperatureStatus;
+            $healthSummary = ($conn | Get-HPEiLOHealthSummary);
+           
+            Log 6 "Querying PowerSupply"
+            $powerSupply = ($conn | Get-HPEiLOPowerSupply);
+            $powerSuppliesDetails = @();
+            foreach ($ps in $powerSupply.PowerSupplies) {
+                $powerSuppliesDetails += @{
+                    SerialNumber = $ps.SerialNumber;
+                    Status       = $iLOVersion -eq 4 ? $ps.Status : $ps.Status.Health;
+                    Model        = $ps.Model;
+                    Name         = $iLOVersion -eq 4 ? $ps.Label : $ps.Name;
+                }
+            }
+            
+            $processor = ($conn | Get-HPEiLOProcessor).Processor;
+            $processorDetails = @();
+            foreach ($pr in $processor) {
+                $processorDetails += @{
+                    Model        = $pr.Model;
+                    SerialNumber = $pr.SerialNumber;    
+                }
             }
 
-            PowerSupply    = @{
-                PowerSystemRedundancy = $powerSupply.PowerSupplySummary.PowerSystemRedundancy;
-                PresentPowerReading = $powerSupply.PowerSupplySummary.PresentPowerReading;
-                PowerSupplies = @(
-                    foreach($s in $powerSupply.PowerSupplies){
-                        return $s.Status;
-                    }
-                )
+            $memory = $iLOVersion -eq 4 ? ($conn | Get-HPEiLOMemoryInfo).MemoryComponent : ($conn | Get-HPEiLOMemoryInfo).MemoryDetails.MemoryData;
+            $memoryDetails = @();
+            foreach ($me in $memory) {
+                $memoryDetails += @{
+                    Location = $iLOVersion -eq 4 ? $me.MemoryLocation.ToString() : $me.DeviceLocator.ToString();
+                    SizeMB   = $iLOVersion -eq 4 ? $me.MemorySizeMB.ToString() : $me.CapacityMiB.ToString();
+                }
+            }            
+
+            Log 0 "$srv querried"
+
+            $srvReport = [ordered]@{
+                Serial         = $findILO.SerialNumber;
+                Part_Type_Name = $conn.TargetInfo.ProductName;
+                Hostname       = ($conn | Get-HPEiLOAccessSetting).ServerName.ToLower();
+                Hostname_Mgnt  = $conn.Hostname.ToLower();
+                MAC_1          = "";
+                MAC_2          = "";
+                MAC_3          = "";
+                MAC_4          = "";
+                Mgnt_MAC       = ($conn | Get-HPEiLOIPv4NetworkSetting).PermanentMACAddress.ToLower();
+
+                Health_Summary = @{
+                    FanStatus           = $healthSummary.FanStatus;
+                    MemoryStatus        = $healthSummary.MemoryStatus;
+                    PowerSuppliesStatus = $healthSummary.PowerSuppliesStatus;
+                    ProcessorStatus     = $healthSummary.ProcessorStatus;
+                    StorageStatus       = $healthSummary.StorageStatus;
+                    TemperatureStatus   = $healthSummary.TemperatureStatus;
+                }
+
+                PowerSupply    = @{
+                    PowerSystemRedundancy = $powerSupply.PowerSupplySummary.PowerSystemRedundancy;
+                    PresentPowerReading   = $powerSupply.PowerSupplySummary.PresentPowerReading;
+                    PowerSupplies         = $powerSuppliesDetails;
+                }
+                
+                Processor      = $processorDetails
+                
+                Memory         = $memoryDetails
+                Network        = ""
+                NetworkAdapter = ""
+                PCIDevices     = ""
+                USBDevices     = ""
+                Devices        = ""
+                Storage        = ""
             }
-            Memory         = ""
-            Processor      = ""
-            Network        = ""
-            NetworkAdapter = ""
-            PCIDevices     = ""
-            USBDevices     = ""
-            Devices        = ""
-            Storage        = ""
+
+        
+            $report += $srvReport;
         }
-
-        $report += $srvReport;
+        Log 0 "Ended"
+        $report | ConvertTo-Json -Depth 10 | Out-File "U:\IPA\IPA\hpeilo_inventoryscript\r.json";
     }
-
-    $report
+    catch {
+        Write-Error $_;
+    }
 }
