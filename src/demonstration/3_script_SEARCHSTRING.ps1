28ba30998409da1be8601ef38bbb97a9742bdc06
wernle_y <yannick.wernle@git.psi.ch>
2025-04-07 16:25:24 +0200

    renamed demonstration-files to match the ppp

---
+++ src/demonstration/3_script_SEARCHSTRING.ps1
@@ -0,0 +1,7 @@
+Remove-Item -Path "U:\IPA\IPA\IPA_Sourcecode_hpeilo_inventoryscript\src\demonstration\dump\*" -Recurse -Force;
+Remove-Item -Path "C:\Users\wernle_y\AppData\Roaming\hpeilo\*" -Recurse -Force;
+Import-Module .\ILO-Inventorizer\ILO-Inventorizer.psm1;
+Import-Module HPEiLOCmdlets;
+Set-ConfigPath -Reset;
+
+Get-HWInfoFromILO -SearchStringInventory "gfa-sioc-cs-de" -Username "Yannik" -Password (ConvertTo-SecureString -String "test!1234" -AsPlainText) -LogLevel 5 -LoggingActivated -DeactivateCertificateValidationILO -ReportPath "U:\IPA\IPA\IPA_Sourcecode_hpeilo_inventoryscript\src\demonstration\dump\" -LogPath "U:\IPA\IPA\IPA_Sourcecode_hpeilo_inventoryscript\src\demonstration\dump\" ;
